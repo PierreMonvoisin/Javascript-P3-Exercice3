f5df3a88a2d543e665c71cb094451156a23ba831
Pierre Monvoisin <monvoisin.pierre@gmail.com>
2019-10-26 15:47:30 +0200

    Exercice 3 non fonctionnel, raison inconnue

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+document.getElementsByClassName("class green").onclick = function() {
+  document.getElementById("text").style.backgroundColor = "#00FF00";
+};
+
+document.getElementsByClassName("class red").onclick = function() {
+  document.getElementById("text").style.backgroundColor = "red";
+};
+
+document.getElementsByClassName("class blue").onclick = function() {
+  document.getElementById("text").style.backgroundColor = "blue";
+};
